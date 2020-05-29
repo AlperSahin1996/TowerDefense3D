ce849487d3e66500be1986b029183fafa84291ab
AlperSahin1996 <alpersahin1996@hotmail.com>
2020-05-30 01:14:06 +0200

    wavespawner gemaakt
    wavespawner gemaakt

---
+++ Assets/Wavespawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wavespawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 10f;
+    private float countdown = 5f;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        Debug.Log("Wave incoming!");
+    }
+}
