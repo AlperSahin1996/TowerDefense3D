cf76d4742520456e52b9140af1822acccdb145ed
AlperSahin1996 <alpersahin1996@hotmail.com>
2020-05-30 00:41:51 +0200

    initiele project voor een towerdefense game
    initiele project voor een towerdefense game

---
+++ Assets/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] WayPoints;
+
+    void Awake()
+    {
+        WayPoints = new Transform[transform.childCount];
+        for (int i = 0; i < WayPoints.Length; i++)
+        { 
+        WayPoints[i] = transform.GetChild(i);
+        }
+    }
+}
